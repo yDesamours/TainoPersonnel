1387711a08d6f94cfccedda9175fdf164f40ab99
dyvelt <dyvelt@tainosystems.com>
2023-10-25 11:34:43 -0400

    commit

--- lib/src/widget/mytile.dart
+++ lib/src/widget/mytile.dart
@@ -5,13 +5,14 @@ import 'package:flutter/material.dart';
 class MyTile extends StatelessWidget {
   void Function()? onTap;
   Widget? icon;
-  String title;
+  String title, subTitle;
   TextStyle? textTheme;
 
   MyTile({
     super.key,
     this.onTap,
     this.title = '',
+    this.subTitle = '',
     this.icon,
     this.textTheme,
   });
@@ -19,14 +20,29 @@ class MyTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
+
+    List<Text> children = [
+      Text(
+        title,
+        style: textTheme ?? theme.textTheme.bodyMedium,
+      ),
+    ];
+
+    if (subTitle.isNotEmpty) {
+      children.add(Text(
+        subTitle,
+        style: theme.textTheme.bodyMedium,
+      ));
+    }
     return Container(
       padding: const EdgeInsets.only(bottom: 5),
       child: ListTile(
         onTap: onTap,
         selectedTileColor: theme.primaryColor,
-        title: Text(
-          title,
-          style: textTheme ?? theme.textTheme.bodySmall,
+        title: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: children,
         ),
         leading: icon,
       ),
